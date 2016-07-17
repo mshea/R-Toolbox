54ae0f124ae7448aa8e9d6807f3a938a595c5d55
Michael E. Shea <mike@mikeshea.net>
2016-07-17 19:30:47 -0400

    Added date by time scatterplot and sample

---
+++ date_time_scatterplot.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(scales)
+d <- read.csv("~/Desktop/tweet_dates.csv")
+#d <- d[1:5000,]
+d$date <- as.Date(d$Parsed.Date, "%Y-%m-%d")
+d$timestring <- strftime(as.POSIXct(d$Parsed.Date, format="%Y-%m-%d %H:%M:%S"), format="%H:%M:%S")
+d$time <- as.POSIXct(d$timestring, format="%H:%M:%S")
+myplot <- ggplot(d, aes(x = date, y=time)) + 
+  geom_point(color="#000000", alpha=.05, size=.5, shape=16)  + 
+  theme(axis.line=element_blank(),
+        #axis.text.x=element_blank(),
+        #axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position="none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_line(colour = "#eeeeee"),
+        panel.grid.minor=element_blank(),
+        plot.background=element_blank(), 
+        strip.background =element_blank()) + 
+        scale_y_datetime(labels=date_format("%I:%M %p EST"))  + 
+  ggtitle("570,773 #dnd Tagged Tweets by Date and Time")
+ggsave("~/Desktop/date_time_scatterplot.jpg", 
+       plot = myplot, 
+       width = 12, height = 6, 
+       dpi = 300)
